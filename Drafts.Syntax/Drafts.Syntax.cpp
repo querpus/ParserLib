c0de398d6b238d0fd703af9a0fbacdb517018ea7
John Ryan Taylor <querpus@gmail.com>
2026-09-08 20:32:12 -0400

    dd

---
+++ Drafts.Syntax/Drafts.Syntax.cpp
@@ -0,0 +1,12 @@
+﻿// Drafts.Syntax.cpp : Defines the entry point for the application.
+//
+
+#include "Drafts.Syntax.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
