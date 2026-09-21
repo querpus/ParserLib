68660a9420034d4f3510021c13659db7d817185d
John Ryan Taylor <querpus@gmail.com>
2026-09-21 06:21:28 -0400

    ghyuhy

---
+++ FormObjectViewer/RegexMaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormObjectViewer;
+
+public partial class RegexMaker : Form
+{
+  public RegexMaker ()
+  {
+    InitializeComponent();
+  }
+}
